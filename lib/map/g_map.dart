66d3ac871ffca7b75713c355e200d8e4f757394f
Raymond0224 <rfk3400@autuni.ac.nz>
2026-05-01 13:20:09 +1200

    Update g_map.dart

--- lib/map/g_map.dart
+++ lib/map/g_map.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 import 'map_api_key.dart';
 
 /*
