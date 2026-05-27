6f019860aef6ac162ef73d85a2d8760ce86ac977
jaydentosul <rcc8141@autuni.ac.nz>
2026-05-28 01:16:23 +1200

    :)

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -45,8 +45,7 @@ class IntroPage extends StatelessWidget {
 
             //Button
             ButtonOne(
-              // onTap: () => Navigator.pushReplacementNamed(context, '/login_page'),
-              onTap: () => Navigator.pushReplacementNamed(context, '/home_page'),
+              onTap: () => Navigator.pushReplacementNamed(context, '/login_page'),
               buttonIcon: Row(
                 mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.center,
